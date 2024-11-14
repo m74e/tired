bd7711e0a2a65437fa34ee23ec7158cd5f346aa4
Muhammed Al-lami <muhammedhn7@gmail.com>
2024-11-14 17:08:45 +0300

    newup

--- src/Pages/web/middelProfile/createnewproject/createnewpro.jsx
+++ src/Pages/web/middelProfile/createnewproject/createnewpro.jsx
@@ -1,44 +1,51 @@
 import React, { useState } from "react";
 import "./createnewpro.css";
-import Upload from "../../../../assets/uploadimg.png"
-import DocumendBracts  from"../../../../assets/documentcode.png"
+
+import DocumendBracts from "../../../../assets/documentcode.png";
 import Arrow from "/public/whitearrow.svg";
 import useStore from "../../../../zustand/store";
 import AddProject from "./addProject.jsx";
 const createnewpro = () => {
-    const[show,setShow]=useState(false);
-    const { count, setCount } = useStore();
-    const openAndCancle=()=>{
-        if(show==false){
-            return setShow(true)
-        }
-        else{
-            return setShow(false)  
-        }
-
+  const [show, setShow] = useState(false);
+  const { count, setCount } = useStore();
+  const openAndCancle = () => {
+    if (show == false) {
+      return setShow(true);
+    } else {
+      return setShow(false);
     }
-    
-    function counterPath() {
-        if (count === 0) {
-          return <AddProject />;
-        } else if (count === 1) {
-          return <Number />;
-        } else if (count === 2) {
-          return <Vaild />;
-        } else if (count === 3) {
-          return <Email />;
-        } else if (count === 4) {
-          return <div></div>;
-        }
-      }
-    
+  };
+
+  function counterPath() {
+    if (count === 0) {
+      return <AddProject />;
+    } else if (count === 1) {
+      return <Number />;
+    } else if (count === 2) {
+      return <Vaild />;
+    } else if (count === 3) {
+      return <Email />;
+    } else if (count === 4) {
+      return <div></div>;
+    }
+  }
+
   return (
     <>
-    <div className="Plusbtn"><button onClick={openAndCancle}><p>+</p></button></div>
+      <div className="Plusbtn">
+        <button onClick={openAndCancle}>
+          <p>+</p>
+        </button>
+      </div>
       <div className={`parantOfnewpro ${show ? "" : "cancle"}`}>
         <div className="bigChild">
           <h2>Create a project</h2>
-          <button className={`redX ${show ? "" : "cancle"}`} onClick={openAndCancle}>X</button>
+          <button
+            className={`redX ${show ? "" : "cancle"}`}
+            onClick={openAndCancle}
+          >
+            X
+          </button>
         </div>
         <div className="TwoLinesGreenOneAndGreyOne">
           <div className="LineGreenOne"></div>
@@ -69,20 +76,26 @@ const createnewpro = () => {
           />
         </div>
         <div className="threebtnboth">
-        <div className="twobtnboth">  <button className="btnForUpload">
-            <img className="btnForUploadImg" src={Upload} alt="" />
-            <p className="btnForUploadPara">media</p>
-          </button>
-          <button className="btnForUpload">
-            <img className="btnForUploadImg" src={DocumendBracts} alt="" />
-            <p className="btnForUploadPara">document</p>
-          </button></div>
-          <button className="NextBtnForUpload" onClick={()=>setCount(count+1)}>
-            <p className="paraNextBtn">Next</p><img className="ArrowForNextBtn" src={Arrow} alt="" />
+          <div className="twobtnboth">
+            {" "}
+            <button className="btnForUpload">
+              <img className="btnForUploadImg" src="" alt="" />
+              <p className="btnForUploadPara">media</p>
+            </button>
+            <button className="btnForUpload">
+              <img className="btnForUploadImg" src={DocumendBracts} alt="" />
+              <p className="btnForUploadPara">document</p>
+            </button>
+          </div>
+          <button
+            className="NextBtnForUpload"
+            onClick={() => setCount(count + 1)}
+          >
+            <p className="paraNextBtn">Next</p>
+            <img className="ArrowForNextBtn" src={Arrow} alt="" />
           </button>
         </div>
       </div>
-      
     </>
   );
 };
